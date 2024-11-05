7fddb3cea791f719444b737ddab14784827cd0fd
Nishant Kumar Verma <nisgyan@gmail.com>
2024-11-05 01:59:10 +0100

    - Add Query and Validator class

---
+++ MarktGuru.Products/MarktGuru.Products.Application/Handlers/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MarktGuru.Products.Domain.Models;
+using MediatR;
+
+namespace MarktGuru.Products.Application.Handlers.Products.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public int Id { get; set; }
+        public GetProductByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
